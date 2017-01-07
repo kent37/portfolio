20cb07d8b8717887c4707552275487fff854e8f4
Kent Johnson <kent3737@gmail.com>
2017-01-06 21:47:35 -0500

    Add a (somewhat lame) spinner when waiting

--- FCC/Weather/js/main.js
+++ FCC/Weather/js/main.js
@@ -3,26 +3,39 @@ $(document).ready(function() {
   
   var refresh = function() {
     if (navigator.geolocation) {
+      // Show spinner; don't use show(), it sets display:block
+      $('#spinner').css('display', 'inline');
+      
+      // Get the current position
       navigator.geolocation.getCurrentPosition(function(position) {
-        var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + 
+        // Use position info
+        var weatherUrl = 
+            'http://api.openweathermap.org/data/2.5/weather?lat=' + 
             position.coords.latitude +
             '&lon=' + position.coords.longitude + '&units=' + units +
             '&APPID=' + '3db83ed2802508059822b511371efa7b';
-        $.getJSON(url, function(weather) {
-          var desc = weather.weather[0].description;
-          desc = desc[0].toUpperCase() + desc.slice(1);
-          $('#at').html('in ' + weather.name);
-          $('#weather').html(desc);
-          $('#temp').html(Math.round(weather.main.temp) + deg);
-          $('#wi').removeClass().addClass('wi wi-owm-'+ weather.weather[0].id);
-          $('#wind').html('Wind ' + Math.round(weather.wind.speed) + ' ' + wind);
-            
-          mapUrl = 'https://www.google.com/maps/embed/v1/view?zoom=14&key=AIzaSyAyioCg5GgtIOV9zcHZaYcUDgP0xTQrZBs&center=' + 
-                position.coords.latitude +
-            ',' + position.coords.longitude;
-        })
+        
+        mapUrl = 'https://www.google.com/maps/embed/v1/view?zoom=14&key=AIzaSyAyioCg5GgtIOV9zcHZaYcUDgP0xTQrZBs&center=' + 
+          position.coords.latitude +
+          ',' + position.coords.longitude;
+
+        // Get the weather
+        $.getJSON(weatherUrl, showWeather)
       });
     }
+  }
+  
+  var showWeather = function(weather) {
+    var desc = weather.weather[0].description;
+    desc = desc[0].toUpperCase() + desc.slice(1);
+    $('#at').html('in ' + weather.name);
+    $('#weather').html(desc);
+    $('#temp').html(Math.round(weather.main.temp) + deg);
+    $('#wi').removeClass().addClass('wi wi-owm-'+ weather.weather[0].id);
+    $('#wind').html('Wind ' + Math.round(weather.wind.speed) + ' ' + wind);
+    
+    // Hide the spinner
+    $('#spinner').hide();
   }
   
   var setImperial = function() {
